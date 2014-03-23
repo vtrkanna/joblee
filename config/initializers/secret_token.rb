4ea8975a1b2a2470934343dfb657c370197fd709
kanna <vtrkanna@gmail.com>
2014-03-24 00:17:43 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Joblee::Application.config.secret_key_base = '5f6538fe76bba2dcd1b2d124d8607a8c06d4adbb6f6d893f6dd4e3888243bf814e71596d0c1b2c19dda24577e10527c10d564d5b9f58e0caa771bfeae0fd3d21'
