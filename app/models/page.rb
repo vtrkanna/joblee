68a85b853ce8f76af82707349b73b94f3bdbada8
kanna <vtrkanna@gmail.com>
2014-03-27 00:20:14 +0530

    initail commit

---
+++ app/models/page.rb
@@ -0,0 +1,5 @@
+class Page
+  include Mongoid::Document
+
+  field :name, :type => String
+end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,17 @@
 class Site
   include Mongoid::Document
+
+  field :name, :type => String
+  field :alt_name, :type => String
+  field :url_id, :type => Integer
+  field :active, :type => Boolean
+  field :page_id, :type => Integer
+  field :status_id, :type => Integer
+  field :tag_id, :type => Integer
+  field :layout, :type => String
+  field :template_id, :type => Integer
+  field :created_by, :type => Integer
+  field :updated_by, :type => Integer
+  field :created_at, :type => DateTime
+  field :updated_at, :type => DateTime
 end
